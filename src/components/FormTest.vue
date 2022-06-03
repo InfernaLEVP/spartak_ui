4494479def15d4f8b085605644046eee1b9e005f
infernalevp <zozulyaevgeny1995@gmail.com>
2022-06-03 15:30:55 +0300

    merge

--- src/components/FormTest.vue
+++ src/components/FormTest.vue
@@ -289,7 +289,7 @@ export default {
                     teamName: this.teamName
                 }
 
-                const response = await fetch("http://45.92.173.81:3020/api/bookGame", {
+                const response = await fetch("https://winliner.ru:8443/api/bookGame", {
                     method: "POST",
                     headers: { "Accept": "application/json", "mode": "no-cors", "Content-Type": "application/json" },
                     body: JSON.stringify(body)
@@ -368,7 +368,7 @@ export default {
                     body.team = this.team;
                 }
 
-                const response = await fetch("http://45.92.173.81:3020/api/bookOneEvent", {
+                const response = await fetch("https://winliner.ru:8443/api/bookOneEvent", {
                     method: "POST",
                     headers: { "Accept": "application/json", "mode": "no-cors", "Content-Type": "application/json" },
                     body: JSON.stringify(body)
