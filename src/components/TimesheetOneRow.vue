e79f4d2a4a8e33cd7871c622c91cf3f612ad1b64
Arseny Petrovich <strangesound@gmail.com>
2022-06-07 17:17:45 +0300

    close08

--- src/components/TimesheetOneRow.vue
+++ src/components/TimesheetOneRow.vue
@@ -200,7 +200,7 @@ export default {
             // ober-items-wrapper
         },
         isDayClosed(day) {
-            if (day.day === '05' || day.day === '06' || day.day === '07') {
+            if (day.day === '05' || day.day === '06' || day.day === '07' || day.day === '08') {
                 return 'closed-day';
             } else {
                 return '';
